e8a3d69d5c42509fb8f40001e3f139ffa97517b4
PXgamer <owzie123@gmail.com>
2017-06-20 09:39:19 +0100

    Updated config to use the HTTP methods from the Calls class
    Added function for getting the client name to send

--- src/Config.php
+++ src/Config.php
@@ -6,12 +6,19 @@ class Config
 {
     const BASE_URL = 'https://haveibeenpwned.com/api/';
     const CURRENT_API_VERSION = 2;
-    const AVAILABLE_HTTP_METHODS = ['GET'];
+    const AVAILABLE_HTTP_METHODS = [
+        Calls::HTTP_MODE_GET
+    ];
 
     private static $clientName = null;
 
     public static function setClientName($sClientName)
     {
         return (self::$clientName = $sClientName);
+    }
+
+    public static function getClientName()
+    {
+        return self::$clientName;
     }
 }
